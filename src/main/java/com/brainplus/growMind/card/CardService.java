f1958c2b8053f945c2a820811c38fb14cc841ba4
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-11-19 00:25:35 +0100

    feat: update card method

--- src/main/java/com/brainplus/growMind/card/CardService.java
+++ src/main/java/com/brainplus/growMind/card/CardService.java
@@ -3,4 +3,6 @@ package com.brainplus.growMind.card;
 public interface CardService {
 
   CardCreationResponse createCardAndAddToDecks(CardCreationRequest request);
+  CardUpdateResponse updateCard(int cardId, CardUpdateRequest request);
+
 }
