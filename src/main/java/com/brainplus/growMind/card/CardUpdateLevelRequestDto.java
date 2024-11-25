646a02461db2294eaf842208378c5b5403cddf4f
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-11-25 23:39:00 +0100

    Added new request DTO for update cards method

--- src/main/java/com/brainplus/growMind/card/CardUpdateLevelRequestDto.java
+++ src/main/java/com/brainplus/growMind/card/CardUpdateLevelRequestDto.java
@@ -1,4 +1,21 @@
 package com.brainplus.growMind.card;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CardUpdateLevelRequestDto {
+
+  @NotNull
+  private Integer id;
+
+  @NotNull
+  private Integer difficulty;
+
 }
