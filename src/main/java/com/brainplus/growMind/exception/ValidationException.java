6bdaa963406ddf1759c649adca8cb7694867031b
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-11-25 22:31:07 +0100

    feat: custom validation error

--- src/main/java/com/brainplus/growMind/exception/ValidationException.java
+++ src/main/java/com/brainplus/growMind/exception/ValidationException.java
@@ -1,5 +1,17 @@
 package com.brainplus.growMind.exception;
 
-public class ValidException extends RuntimeException {
-  
+import lombok.Getter;
+
+import java.util.Set;
+
+@Getter
+public class ValidationException extends RuntimeException {
+
+  private final Set<String> violations;
+
+  public ValidationException(Set<String> violations) {
+    super("Validation failed");
+    this.violations = violations;
+  }
+
 }
